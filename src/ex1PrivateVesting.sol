7bdb7ba12ec47c276a4f2e20a2f701a8d658f38d
hemang-h <hemangcertificate@gmail.com>
2025-02-01 00:54:10 +0530

    ✨ Add PrivateVesting contract with upgradeable functionality

---
+++ src/ex1PrivateVesting.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier:MIT
+
+pragma solidity ^0.8.23;
+
+import {AccessControlUpgradeable} from "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
+import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/utils/ReentrancyGuardUpgradeable.sol";
+
+import "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
+
+contract PrivateVesting is Initializable, AccessControlUpgradeable, UUPSUpgradeable {
+    bytes32 public constant UPGRADER_ROLE = keccak256("UPGRADER_ROLE");
+    
+    constructor() {
+        _disableInitializers();
+    }
+
+    function initialise() public initializer {
+        __AccessControl_init();
+        __UUPSUpgradeable_init();
+    }
+
+    function _authorizeUpgrade(address newImplementation)
+        internal
+        onlyRole(UPGRADER_ROLE)
+        override
+    {}
+}
