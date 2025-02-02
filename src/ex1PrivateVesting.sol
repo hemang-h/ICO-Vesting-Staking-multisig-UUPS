00c8296ba504d2c4f70f1d5ad484743297774160
hemang-h <hemangcertificate@gmail.com>
2025-02-02 22:13:29 +0530

    🚧 Added Functions layout

--- src/ex1PrivateVesting.sol
+++ src/ex1PrivateVesting.sol
@@ -11,6 +11,8 @@ import "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
 
 contract PrivateVesting is Initializable, AccessControlUpgradeable, UUPSUpgradeable {
     bytes32 public constant UPGRADER_ROLE = keccak256("UPGRADER_ROLE");
+    bytes32 public constant VESTING_CREATOR_ROLE = keccak256("VESTING_CREATOR_ROLE");
+    bytes32 public constant OWNER_ROLE = keccak256("OWNER_ROLE");
     
     struct Vesting {
         address beneficiary;
@@ -27,6 +29,23 @@ contract PrivateVesting is Initializable, AccessControlUpgradeable, UUPSUpgradea
     function initialise() public initializer {
         __AccessControl_init();
         __UUPSUpgradeable_init();
+    }
+
+    function createVesting(
+        address _beneficary,
+        uint256 _totalAmount,
+        uint256 _startTime,
+        uint256 _endTime
+    ) external onlyRole(VESTING_CREATOR_ROLE) {
+
+    }
+
+    function updateVesting() external onlyRole(VESTING_CREATOR_ROLE){
+
+    }
+
+    function setEx1TokenSaleContract() external onlyRole(OWNER_ROLE) {
+
     }
 
     function _authorizeUpgrade(address newImplementation)
